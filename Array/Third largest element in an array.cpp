0912eab42d28a1bf97952d9f9e6248e042742b8d
Bhavesh Alawane <bhaveshalawane96@gmail.com>
2026-08-08 23:02:34 +0530

    Add function to find third largest element in array
    Implement a function to find the third largest element in an array. Returns -1 if there are less than three elements.

---
+++ Array/Third largest element in an array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int thirdLargest(vector<int> &arr) {
+        int n= arr.size();
+        
+        if(n<3) return -1;
+        
+        int first=-1, second=-1, third=-1;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]>first){
+                third=second;
+                second= first;
+                first= arr[i];
+            }
+            else if(arr[i]>second){
+                third=  second;
+                second= arr[i];
+            }
+            else if(arr[i]>third){
+                third= arr[i];
+            }
+        }
+        return third;
+        
+    }
+};
